ad2d25874fbb51719dd1b213e7b39606d9986a47
Akash Kinkar Pandey <akashkinkarpandey@gmail.com>
2025-02-02 18:49:16 +0530

    SOCKET created

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
-#include <iostream>     // Include input-output stream library
-#include <WinSock2.h>   // Include Windows Sockets 2 library
+#include <iostream>   // Include input-output stream library
+#include <WinSock2.h> // Include Windows Sockets 2 library
+#include <WS2tcpip.h> // Include Windows Sockets 2 TCP/IP library
+#include <tchar.h>    // Include tchar.h header file
+using namespace std;  // Use the standard namespace
 
-using namespace std;    // Use the standard namespace
-
-#pragma comment(lib, "ws2_32.lib")  // Link with ws2_32.lib library
+#pragma comment(lib, "ws2_32.lib") // Link with ws2_32.lib library
 
 // Function to initialize WinSock
 bool Initialize()
 {
-    WSADATA data;  // Structure to store WinSock data
+    WSADATA data; // Structure to store WinSock data
     // Initialize WinSock version 2.2
     return WSAStartup(MAKEWORD(2, 2), &data) == 0;
 }
@@ -18,11 +19,19 @@ int main()
     // Check if WinSock initialization was successful
     if (!Initialize())
     {
-        cout << "winsock initialization failed" << endl;  // Print error message
-        return 1;  // Exit program
+        cout << "winsock initialization failed" << endl; // Print error message
+        return 1;                                        // Exit program
     }
 
-    cout << "Hello Socket Programming!";  // Print success message
-   
-    return 0;  // Exit program
+    cout << "Hello Socket Programming!"; // Print success message
+    // Create a TCP socket for IPv4 addresses
+    SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenSocket == INVALID_SOCKET)
+    {
+        cout << "socket creation failed" << endl; // Print error message
+        WSACleanup();                             // Clean up Winsock
+        return 1;                                 // Exit program
+    }
+    
+    return 0; // Exit program
 }
